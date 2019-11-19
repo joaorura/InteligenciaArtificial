499f5bcb7da201febce35ef8029c16a94155a2dd
João Messias Lima Pereira <jmlp@ic.ufal.br>
2019-11-19 01:01:26 -0300

    ...

--- Problema do metrô de Paris/main.py
+++ Problema do metrô de Paris/main.py
@@ -1,7 +1,8 @@
 from utils import check_type
+from copy import deepcopy
 
 
-heuristic = [
+data = [
     [0, 11, 20, 27, 40, 43, 39, 28, 18, 10, 18, 30, 30, 32],
     [11, 0, 9, 16, 29, 32, 28, 19, 11, 4, 17, 23, 21, 24],
     [20, 9, 0, 7, 20, 22, 19, 15, 10, 11, 21, 21, 13, 18],
@@ -18,23 +19,42 @@ heuristic = [
     [32, 24, 18, 17, 20, 20, 17, 30, 28, 23, 39, 37, 5, 0]
 ]
 
+test = [
+    ['W', 'B', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
+    ['B', 'W', 'B', 'W', 'W', 'W', 'W', 'W', 'Y', 'Y', 'W', 'W', 'W', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'W', 'W', 'W', 'R', 'W', 'W', 'W', 'R', 'W'],
+    ['W', 'W', 'B', 'W', 'B', 'W', 'W', 'G', 'W', 'W', 'W', 'W', 'G', 'W'],
+    ['W', 'W', 'W', 'B', 'W', 'B', 'Y', 'Y', 'W', 'W', 'W', 'W', 'W', 'W'],
+    ['W', 'W', 'W', 'W', 'B', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
+    ['W', 'W', 'W', 'W', 'Y', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
+    ['W', 'W', 'W', 'G', 'Y', 'W', 'W', 'W', 'Y', 'W', 'W', 'G', 'W', 'W'],
+    ['W', 'Y', 'R', 'W', 'W', 'W', 'W', 'Y', 'W', 'W', 'R', 'W', 'W', 'W'],
+    ['W', 'Y', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
+    ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'R', 'W', 'W', 'W', 'W', 'W'],
+    ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'G', 'W', 'W', 'W', 'W', 'W', 'W'],
+    ['W', 'W', 'R', 'G', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'G'],
+    ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'G', 'W']
+]
 
 class Problem:
-    def __init__(self, station_initial, station_final, amount_stations, heuristic):
+    def __init__(self, station_initial, station_final, amount_lines, lines, heuristic):
         check_type(int, station_initial)
         check_type(int, station_final)
-        check_type(int, amount_stations)
+        check_type(int, amount_lines)
 
         check_type(list, heuristic)
-        if len(heuristic) == 0:
+        if len(heuristic) != 0:
             raise ValueError
         check_type(list, heuristic[0])
 
         self.station_initial = station_initial
         self.station_final = station_final
-        self.amount_stations = amount_stations
+        self.amount_lines = amount_lines
         self.heuristic = heuristic
         self.result = None
+
+    def _start(self):
+        pass
 
     def get_result(self):
         if self.result is None:
@@ -44,4 +64,10 @@ class Problem:
 
 
 def main():
-    problem = Problem(0, 3, 4, heuristic[0:3][0:3])
+    pass
+    # problem = Problem(0, 3, 4, data)
+    # print(problem.get_result())
+
+
+if __name__ == '__main__':
+    main()
